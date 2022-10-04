84db22131c32f2a494a6dbab8920e59c0e2e911f
vinaygupta2255 <vinay.gupta2255@gmail.com>
2022-10-05 00:30:02 +0530

    Time: 25 ms (46.60%), Space: 43.4 MB (55.08%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len=s1.length();
+        if(len>s2.length()){
+            return false;
+        }
+        
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        
+        //Solving 1 case
+        for(int i=0;i<len;i++){
+            arr1[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<s2.length()-len;i++){
+            if(Arrays.equals(arr1,arr2)){
+                return true;
+            }
+            arr2[s2.charAt(i+len)-'a']++;// one by one sliding the character
+            arr2[s2.charAt(i)-'a']--;
+        }
+        
+        return Arrays.equals(arr1,arr2);
+    }
+}
