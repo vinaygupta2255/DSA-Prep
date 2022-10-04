a68b7816021d945e2bc04765346e5b374d10b674
vinaygupta2255 <vinay.gupta2255@gmail.com>
2022-10-05 00:29:56 +0530

    Time: 2 ms (73.80%), Space: 43.8 MB (64.33%) - LeetHub

---
+++ 443-string-compression/443-string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        
+        int count = 0;
+                
+        for(int i = 0; i<chars.length; i++){
+            int val = 1;
+            while(i != chars.length-1 && chars[i] == chars[i+1]){
+                i++;
+                val++;
+            }
+            
+            chars[count++] = chars[i];
+            if(val == 1)
+                continue;
+            
+            String st = String.valueOf(val);
+            for(int k = 0;k<st.length();k++){
+                chars[count++] = st.charAt(k);
+                
+            }
+        }
+        return count;
+    }
+}
