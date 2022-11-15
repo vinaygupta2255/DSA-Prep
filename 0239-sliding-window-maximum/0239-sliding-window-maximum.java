e53d965f9fc9bc8c23fabd5e987f0500b8a69728
Vinay Gupta <vinay.gupta2255@gmail.com>
2022-11-15 21:38:09 +0530

    Time: 74 ms (58.40%), Space: 155.2 MB (16.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,83 +1,29 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
-                q.removeLast();
-            }
-            q.addLast(i);
-            // remove first element if it's outside the window
-            if (q.getFirst() == i - k) {
-                q.removeFirst();
-            }
-            // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
-            if (i >= k - 1) {
-                res.add(nums[q.peek()]);
-            }
-        }
-        return res.stream().mapToInt(i->i).toArray();            
-    }
-}
+	
+	// same we can try using a arr[] only
+	// Also, priorityQueue will not work as we can take head as max element but how we can 	 
+	// remove the element from last window while doing window slide
+    // understand this solution deeply
+	public static int[] maxSlidingWindow(int[] nums, int k) {
+		int len = nums.length;
+		Deque<Integer> dq = new ArrayDeque<>(); 
+		int[] ans = new int[nums.length-k+1];
 
-
-
-
-/* my own solution which is throwing time limit exceeded
-
-class Solution {
-public static int[] maxSlidingWindow(int[] nums, int k) {
-	int n = nums.length;
-	Deque<Integer> dq = new ArrayDeque<>(); 
-	int[] ans = new int[nums.length-k+1];int temp = 0;
-	for(int i=0; i<k; i++) {
-		if(nums[i] > nums[temp]) {
-			temp = i;
-		}
-	}
-	dq.addLast(temp);//adding max element of window k
-
-	//store answer of first k size window
-	if(dq.size() > 0) {
-		ans[0] = nums[dq.peekFirst()];
-	}
-	else
-	{
-		ans[0] = 0;
-	}
-
-	//now process for remaining windows
-	for(int i = k, j=1; i<n; i++, j++) {
-
-		//first pop, out of window element
-		if(!dq.isEmpty() && (i - dq.getFirst())>=k ) {
-			dq.pop();
-		}
-
-		//then push current element
-		if(dq.isEmpty()) {// push max element of window k
-			int temp1 = i-k+1;
-			for(int s=i-k+1; s<=i; s++) {
-				if(nums[s] > nums[temp1]) {
-					temp1 = s;
-				}
+		for(int i = 0;i<len;i++) {
+		// tricky line, deleting all numbers from back of queue which are lesser than current adding element
+			while(!dq.isEmpty() && nums[dq.getLast()]<=nums[i]) {
+				dq.removeLast();
 			}
-			dq.addLast(temp1);//adding max element of window k
-		}else if(!dq.isEmpty() && nums[i] > nums[dq.getFirst()]) {
-			dq.pop();
-			dq.push(i);
+			dq.add(i);
+			if(dq.peekFirst() == i-k) {
+				dq.removeFirst();
+			}
+			// if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+			if(i>=k-1) {
+				ans[i-(k-1)] = nums[dq.peek()];
+			}
 		}
-
-		// put in ans
-		if(dq.size() > 0) {
-			ans[j] = nums[dq.getFirst()];
-		}
-		else
-		{
-			ans[j] = 0;
-		}
+		return ans;
 	}
-	return ans;
+
 }
-}
-*/
